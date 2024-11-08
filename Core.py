013522719413c96cce1bc5f458a42b35152d5924
Rafayel Latif <74052861+Rafipilot@users.noreply.github.com>
2024-11-08 17:27:20 +0000

    Update Core.py
    Updating comments

--- Core.py
+++ Core.py
@@ -3,26 +3,24 @@ class WeightlessNeuron:
         self.memory_inputs = []
         self.memory_outputs = []
 
-    def train(self, input, output):
+    def train(self, input, output):# Adding the Input output pairs to lists
         # Store the input and corresponding output in memory
         self.memory_inputs.append(input)
         self.memory_outputs.append(output)
 
-    def hamming_distance(self, input1, input2):
+    def hamming_distance(self, input1, input2): #Calculate hamming distance
         if len(input1) != len(input2):
             print("Error: input shapes don't match with stored memory")
             raise ValueError("Inputs must have the same length.")
 
-        # Correctly calculate Hamming distance
         hamming_distance = 0
-        for i in range(len(input1)):  # Compare corresponding elements
+        for i in range(len(input1)): 
             if input1[i] != input2[i]:
-                hamming_distance += 1
+                hamming_distance += 1 #Add 1 to the distance if we have a diffrence
         return hamming_distance
 
     def predict(self, input):
-        # Check if the input is in memory_inputs
-        distances = []
+        distances = [] # Initialising list to temporary hold the distance between the Input and each stored Input
         
         for stored_input in self.memory_inputs:
             hamming_dist = self.hamming_distance(input, stored_input)
@@ -34,9 +32,8 @@ class WeightlessNeuron:
         
         # Return the corresponding output from memory_outputs
         output = self.memory_outputs[index]
-        return output
+        return 
 
-# Create an instance of the weightless neuron
 neuron = WeightlessNeuron()
 
 # Train the neuron with input-output pairs
